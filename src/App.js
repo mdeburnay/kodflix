3fda6ac0896f0bf647c31d10f7e798e02ac65cff
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-05-04 16:29:59 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
