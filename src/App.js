3d6015c80a257c3aff2da9c0cb1edc6666c6e770
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-05-05 13:15:49 +0100

    Added TV shows on each row & added overlay

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-import kodflixLogo from "./images/kodflixLogo.png";
+import darkKnightLogo from "./images/darkKnightLogo.jpg";
+import fightClubLogo from "./images/fightClubLogo.jpg";
+import inherentViceLogo from "./images/inherentViceLogo.jpg";
+import requiemForADreamLogo from "./images/requiemForADreamLogo.jpg";
+import blacKKKlansmanLogo from "./images/blackKKlansmanLogo.jpg";
+import shawshankRedemptionLogo from "./images/shawshankRedemptionLogo.jpg";
 
-function App() {
-  return (
-    <div>
-      <img src={kodflixLogo} alt="Main Kodflix Logo" />
-      <br />
-      <br />
-      <br />
-      <div className="container">
-        <div className="movie-poster">Inherent Vice</div>
-        <div className="movie-poster">Shawshank Redemption</div>
-        <div className="movie-poster">The Dark Knight</div>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="movie-poster">
+            <img src={darkKnightLogo} alt="Dark Knight Poster" />
+            <div className="overlay">
+              <h2>The Dark Knight</h2>
+            </div>
+          </div>
+          <div className="movie-poster">
+            <img src={fightClubLogo} alt="Fight Club Poster" />
+            <div className="overlay">
+              <h2>Fight Club</h2>
+            </div>
+          </div>
+          <div className="movie-poster">
+            <img src={inherentViceLogo} alt="Inherent Vice Poster" />
+            <div className="overlay">
+              <h2>Inherent Vice</h2>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="movie-poster">
+            <img src={requiemForADreamLogo} alt="Requiem For A Dream Poster" />
+            <div className="overlay">
+              <h2>Requiem For A Dream</h2>
+            </div>
+          </div>
+          <div className="movie-poster">
+            <img src={blacKKKlansmanLogo} alt="BlacKkKlansman Poster" />
+            <div className="overlay">
+              <h2>BlacKKKlansman</h2>
+            </div>
+          </div>
+          <div className="movie-poster">
+            <img
+              src={shawshankRedemptionLogo}
+              alt="Shawshank Redemption Poster"
+            />
+            <div className="overlay">
+              <h2>The Shawshank Redemption</h2>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="container">
-        <div className="movie-poster">Fight Club</div>
-        <div className="movie-poster">Requiem For A Dream</div>
-        <div className="movie-poster">The Other Guys</div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
