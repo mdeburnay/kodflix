f4967d68dd2a9299bf8686e2f9e356e9e20fde05
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-05-05 08:39:45 +0100

    logo width 100% and added 6 movie titles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import "./App.css";
+
+import kodflixLogo from "./images/kodflixLogo.png";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <img src={kodflixLogo} alt="Main Kodflix Logo" />
+      <br />
+      <br />
+      <br />
+      <div className="container">
+        <div className="movie-poster">Inherent Vice</div>
+        <div className="movie-poster">Shawshank Redemption</div>
+        <div className="movie-poster">The Dark Knight</div>
+      </div>
+      <div className="container">
+        <div className="movie-poster">Fight Club</div>
+        <div className="movie-poster">Requiem For A Dream</div>
+        <div className="movie-poster">The Other Guys</div>
+      </div>
     </div>
   );
 }
