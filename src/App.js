bc8d691165840600117c8b8d70ef19e9d86f47d3
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-05-21 11:12:19 +0100

    Added navbar and summary text

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import MovieList from "../src/Pages/MovieList";
-import Navbar from "./components/Navbar";
-=======
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import MovieList from "../src/Pages/MovieList";
-import Navbar from "./components/Navbar";
-import MovieId from "./components/MovieId";
-import NotFound from "../src/Pages/NotFound";
->>>>>>> 20fa336e531f44c126ae01b5773e58d0acbba583
+import MovieList from "../src/Pages/MovieList.js";
+import Navbar from "./components/Navbar.js";
+import MovieDetails from "./components/MovieDetails/MovieDetails.js";
+import NotFound from "../src/Pages/NotFound.js";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <Navbar />
-        <MovieList />
-      </div>
-=======
       <Router>
         <div className="App">
           <Navbar />
           <Switch>
             <Route exact path="/" component={MovieList} />
             <Route exact path="/not-found" component={NotFound} />
-            <Route exact path="/:movieId" component={MovieId} />
+            <Route exact path="/:movieId" component={MovieDetails} />
           </Switch>
         </div>
       </Router>
->>>>>>> 20fa336e531f44c126ae01b5773e58d0acbba583
     );
   }
 }
---
+++ src/components/MovieDetails/MovieDetails.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import getMovieList from "../getMovieList";
+
+import "./MovieDetails.css";
+
+export default class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = { movie: {} };
+  }
+
+  componentDidMount() {
+    let movieId = this.props.match.params.movieId;
+    let movie = getMovieList().find((movie) => movie.id === movieId);
+    this.setState({ movie: movie });
+  }
+
+  render() {
+    if (this.state.movie === undefined) {
+      return <Redirect to="/not-found" />;
+    } else {
+      return (
+        <div className="movie-details">
+          <h1 className="movie-title">{this.state.movie.title}</h1>
+          <div className="details-container">
+            <div className="movie-synopsis">{this.state.movie.details}</div>
+            <img
+              className="movie-details-image"
+              src={this.state.movie.logo}
+              alt={this.state.movie.title}
+            />
+          </div>
+          <Link className="home-button" to="/">
+            Home
+          </Link>
+        </div>
+      );
+    }
+  }
+}
