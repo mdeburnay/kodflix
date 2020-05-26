3caa22277887af41aa34fca32e5deda6fcee4081
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-05-26 08:41:37 +0100

    Removed package-lock to fix Heroku, unsuccessful

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const movies = require("./backendMovieList");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/rest/movieList", (req, res) => res.send(movies.movieList));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
