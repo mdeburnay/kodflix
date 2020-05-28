f596547c0e2b5dd5bd6226c840794733b5c27d63
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-05-28 08:07:55 +0100

    used nodemon in backend

---
+++ public/playground.js
@@ -0,0 +1,9 @@
+// function getAverage(values) {
+//   return values.reduce((total, number) => {
+//     return total + number / values.length;
+//   }, 0);
+// }
+
+// console.log(getAverage([1, 2, 3]));
+
+
