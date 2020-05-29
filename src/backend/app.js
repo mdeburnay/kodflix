4d2533e9c098f883e39393f25ddb017b24485040
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-05-29 16:01:44 +0100

    Added fetch to package.json

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 3001;
 const movies = require("./backendMovieList");
 
+fetch("http://localhost:3001/movieList")
+.then((response) => response.json())
+.then((data) => console.log(data));
+
 app.get("/movieList", (req, res) => res.send(movies.movieList));
 
 app.listen(port, () =>
