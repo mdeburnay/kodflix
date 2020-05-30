21cb657d40a995475d8c9ed723123a5edf7f8cab
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-05-30 16:46:26 +0100

    Linked frontend with backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const movies = require("./backendMovieList");
+const fetch = require("node-fetch");
 
-fetch("http://localhost:3001/movieList")
-.then((response) => response.json())
-.then((data) => console.log(data));
+fetch("http://localhost:3001/api/movieList")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
 app.get("/movieList", (req, res) => res.send(movies.movieList));
 
