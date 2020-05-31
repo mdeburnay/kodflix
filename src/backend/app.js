750c72ad7679de22c817da12bb90d6b3cb46fa4a
Max De Burnay-Bastos <48160441+mdeburnay@users.noreply.github.com>
2020-05-31 14:07:57 +0100

    added 'backend' and 'frontend' folders (#3)
    Co-authored-by: MdBB <48160441+maximoose773@users.noreply.github.com>

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const path = require("path");
+const port = process.env.PORT || 3001;
 const movies = require("./backendMovieList");
 const fetch = require("node-fetch");
+
+app.use(express.static(path.join(__dirname, "../../build")));
 
 fetch("http://localhost:3001/api/movieList")
   .then((response) => response.json())
@@ -10,6 +13,12 @@ fetch("http://localhost:3001/api/movieList")
 
 app.get("/movieList", (req, res) => res.send(movies.movieList));
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../../build", "index.html"));
+});
+
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
+
+
